807322edcf66fbac54662260b523f3933f9f66a9
Mark Johnson <iggtastic@gmail.com>
2017-06-02 14:28:45 -0700

    Customer Discount Rate Display Showcasing Eager Class Loading

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,16 +8,45 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        // this is how to establish a connection to a database in the model
+        // we need to
+        //   1 - declare a private variable to store the ApplicationDbContext instance
+        //   2 - initialize that instance variable in the class constructor
+        //   3 - dispose of the instance variable in an overrided Dispose method
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View(GetCustomers());
+            // this gets all customers from the database
+            // the database is not accessed here, but rather in the view while looping
+            // need to use the Include method to pull in the MembershipType class since
+            //   Customer is dependent on it and it will not be loaded by MVC by default
+            //   in the View. the View will only pull in the explicitly referenced class,
+            //   Customer. Basically, we're tying Customer to MembershipType.
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+            //return View(GetCustomers());
         }
 
         public ActionResult Details(int id)
         {
             // gets the customer matching the provided id
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            // note that this query will be executed against the db immediately
+            //   due to use of the Linq method, SingleOrDefault
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -24,12 +54,14 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IList<Customer> GetCustomers()
-        {
-            return new List<Customer> {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
-        }
+        // this method is not required anymore since we're using the database instead
+        //   of a static list
+        //private IList<Customer> GetCustomers()
+        //{
+        //    return new List<Customer> {
+        //        new Customer { Id = 1, Name = "John Smith" },
+        //        new Customer { Id = 2, Name = "Mary Williams" }
+        //    };
+        //}
     }
 }
