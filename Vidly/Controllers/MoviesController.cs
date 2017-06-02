8da377f5b745b5c6e4dce2352e1701d2ad03d86c
Mark Johnson <iggtastic@gmail.com>
2017-06-02 13:44:54 -0700

    Status Just Prior To Using Data From Database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -8,47 +8,6 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var customers = new List<Customer>() {
-                new Customer { Name = "bob" },
-                new Customer { Name = "larry" }
-            };
-            var movie = new Movie() { Name = "Shrek" };
-
-            // use a viewmodel when you need to include data from 2 or more
-            //   different models since only 1 model can be imported in a view
-            var viewModel = new RandomMovieViewModel { Movie = movie, Customers = customers };
-
-            // this is a bad approach to passing data to views since it's fragile
-            //ViewData["Movie"] = movie;
-            //return View();
-
-            // this is the preferred method of passing data to views
-            //   since we can access the model properties directly in the view
-            //return View(movie);
-
-            // when using a viewmodel, pass the viewmodel to the view instead of the model
-            return View(viewModel);
-
-            // the View method will assign the variable 'movie' to an instance
-            //   of ViewResult in its ViewData proprty, in its Model property,
-            //   like this: viewResult.ViewData.Model
-
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
-
-
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
         // movies
         public ActionResult Index()
         {
@@ -72,13 +31,6 @@ namespace Vidly.Controllers
                 new Movie { Name = "Shrek", Id = 1 },
                 new Movie { Name = "Fargo", Id = 2 }
             };
-        }
-
-        // sample attribute route with regex constraints
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month)
-        {
-            return Content(year + "/" + month);
         }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,8 +1,17 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required] // this annotation says that the Name property is no longer nullable
+        [StringLength(255)] // this annotation says that the Name property has a max char length of 255
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        // by convention, entity framework treats this naming syntax
+        //   as a db foreign key to the primary key (Id) of MembershipType
+        public byte MembershipTypeId { get; set; }
     }
 }
